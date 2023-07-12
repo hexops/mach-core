04d8be9303c50e444adc4b5b8233072f10edbcfc
Stephen Gutekanst <stephen@hexops.com>
2023-07-12 10:24:48 -0700

    remove more old libmachcore references
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,3 @@ pub const gpu = @import("gpu");
 pub const sysjs = @import("sysjs");
 const builtin = @import("builtin");
 pub const platform_util = if (builtin.cpu.arch == .wasm32) {} else @import("platform/native/util.zig");
-
-test {
-    _ = @import("platform/libmachcore.zig");
-    _ = @import("platform/libmachcore_app.zig");
-}
