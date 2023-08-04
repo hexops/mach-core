0f09ae8e529da7a1a9d1212405e8377553687a62
Stephen Gutekanst <stephen@hexops.com>
2023-08-03 20:39:33 -0700

    remove all submodules (use package manager)
    Helps hexops/mach#903
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const glfw = @import("mach_glfw");
-const gpu_dawn = @import("libs/mach-gpu-dawn/build.zig");
-const gpu = @import("libs/mach-gpu/build.zig").Sdk(.{
+const gpu_dawn = @import("mach_gpu_dawn");
+const gpu = @import("mach_gpu").Sdk(.{
     .gpu_dawn = gpu_dawn,
 });
 pub const core = @import("build.zig").Sdk(.{
@@ -24,7 +24,7 @@ pub fn build(b: *std.Build) !void {
 }
 
 fn glfwLink(b: *std.Build, step: *std.build.CompileStep) void {
-    const glfw_dep = b.dependency("mach_glfw", .{
+    const glfw_dep = b.dependency("mach_gpu.mach_gpu_dawn.mach_glfw", .{
         .target = step.target,
         .optimize = step.optimize,
     });
@@ -34,7 +34,7 @@ fn glfwLink(b: *std.Build, step: *std.build.CompileStep) void {
     // TODO(build-system): Zig package manager currently can't handle transitive deps like this, so we need to use
     // these explicitly here:
     @import("glfw").addPaths(step);
-    if (step.target.toTarget().isDarwin()) xcode_frameworks.addPaths(b, step);
+    if (step.target.toTarget().isDarwin()) @import("xcode_frameworks").addPaths(b, step);
     step.linkLibrary(b.dependency("vulkan_headers", .{
         .target = step.target,
         .optimize = step.optimize,
@@ -72,7 +72,7 @@ pub fn Sdk(comptime deps: anytype) type {
                 .source_file = .{ .path = sdkPath("/src/main.zig") },
                 .dependencies = &.{
                     .{ .name = "gpu", .module = deps.gpu.module(b) },
-                    .{ .name = "glfw", .module = b.dependency("mach_glfw", .{
+                    .{ .name = "glfw", .module = b.dependency("mach_gpu.mach_gpu_dawn.mach_glfw", .{
                         .target = target,
                         .optimize = optimize,
                     }).module("mach-glfw") },
@@ -93,7 +93,7 @@ pub fn Sdk(comptime deps: anytype) type {
             while (iter.next()) |e| {
                 main_tests.addModule(e.key_ptr.*, e.value_ptr.*);
             }
-            main_tests.addModule("glfw", b.dependency("mach_glfw", .{
+            main_tests.addModule("glfw", b.dependency("mach_gpu.mach_gpu_dawn.mach_glfw", .{
                 .target = target,
                 .optimize = optimize,
             }).module("mach-glfw"));
@@ -183,7 +183,7 @@ pub fn Sdk(comptime deps: anytype) type {
                         });
                         // TODO(core): figure out why we need to disable LTO: https://github.com/hexops/mach/issues/597
                         exe.want_lto = false;
-                        exe.addModule("glfw", b.dependency("mach_glfw", .{
+                        exe.addModule("glfw", b.dependency("mach_gpu.mach_gpu_dawn.mach_glfw", .{
                             .target = exe.target,
                             .optimize = exe.optimize,
                         }).module("mach-glfw"));
@@ -249,116 +249,6 @@ pub fn Sdk(comptime deps: anytype) type {
         };
     };
 }
-
-// TODO(build-system): This is a workaround that we copy anywhere xcode_frameworks needs to be used.
-// With the Zig package manager, it should be possible to remove this entirely and instead just
-// write:
-//
-// ```
-// step.linkLibrary(b.dependency("xcode_frameworks", .{
-//     .target = step.target,
-//     .optimize = step.optimize,
-// }).artifact("xcode-frameworks"));
-// @import("xcode_frameworks").addPaths(step);
-// ```
-//
-// However, today this package cannot be imported with the Zig package manager due to `error: TarUnsupportedFileType`
-// which would be fixed by https://github.com/ziglang/zig/pull/15382 - so instead for now you must
-// copy+paste this struct into your `build.zig` and write:
-//
-// ```
-// try xcode_frameworks.addPaths(b, step);
-// ```
-const xcode_frameworks = struct {
-    pub fn addPaths(b: *std.Build, step: *std.build.CompileStep) void {
-        // branch: mach
-        xEnsureGitRepoCloned(b.allocator, "https://github.com/hexops/xcode-frameworks", "723aa55e9752c8c6c25d3413722b5fe13d72ac4f", xSdkPath("/zig-cache/xcode_frameworks")) catch |err| @panic(@errorName(err));
-
-        step.addFrameworkPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/Frameworks") });
-        step.addSystemIncludePath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/include") });
-        step.addLibraryPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/lib") });
-    }
-
-    fn xEnsureGitRepoCloned(allocator: std.mem.Allocator, clone_url: []const u8, revision: []const u8, dir: []const u8) !void {
-        if (xIsEnvVarTruthy(allocator, "NO_ENSURE_SUBMODULES") or xIsEnvVarTruthy(allocator, "NO_ENSURE_GIT")) {
-            return;
-        }
-
-        xEnsureGit(allocator);
-
-        if (std.fs.openDirAbsolute(dir, .{})) |_| {
-            const current_revision = try xGetCurrentGitRevision(allocator, dir);
-            if (!std.mem.eql(u8, current_revision, revision)) {
-                // Reset to the desired revision
-                xExec(allocator, &[_][]const u8{ "git", "fetch" }, dir) catch |err| std.debug.print("warning: failed to 'git fetch' in {s}: {s}\n", .{ dir, @errorName(err) });
-                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
-                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
-            }
-            return;
-        } else |err| return switch (err) {
-            error.FileNotFound => {
-                std.log.info("cloning required dependency..\ngit clone {s} {s}..\n", .{ clone_url, dir });
-
-                try xExec(allocator, &[_][]const u8{ "git", "clone", "-c", "core.longpaths=true", clone_url, dir }, ".");
-                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
-                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
-                return;
-            },
-            else => err,
-        };
-    }
-
-    fn xExec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
-        var child = std.ChildProcess.init(argv, allocator);
-        child.cwd = cwd;
-        _ = try child.spawnAndWait();
-    }
-
-    fn xGetCurrentGitRevision(allocator: std.mem.Allocator, cwd: []const u8) ![]const u8 {
-        const result = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &.{ "git", "rev-parse", "HEAD" }, .cwd = cwd });
-        allocator.free(result.stderr);
-        if (result.stdout.len > 0) return result.stdout[0 .. result.stdout.len - 1]; // trim newline
-        return result.stdout;
-    }
-
-    fn xEnsureGit(allocator: std.mem.Allocator) void {
-        const argv = &[_][]const u8{ "git", "--version" };
-        const result = std.ChildProcess.exec(.{
-            .allocator = allocator,
-            .argv = argv,
-            .cwd = ".",
-        }) catch { // e.g. FileNotFound
-            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
-            std.process.exit(1);
-        };
-        defer {
-            allocator.free(result.stderr);
-            allocator.free(result.stdout);
-        }
-        if (result.term.Exited != 0) {
-            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
-            std.process.exit(1);
-        }
-    }
-
-    fn xIsEnvVarTruthy(allocator: std.mem.Allocator, name: []const u8) bool {
-        if (std.process.getEnvVarOwned(allocator, name)) |truthy| {
-            defer allocator.free(truthy);
-            if (std.mem.eql(u8, truthy, "true")) return true;
-            return false;
-        } else |_| {
-            return false;
-        }
-    }
-
-    fn xSdkPath(comptime suffix: []const u8) []const u8 {
-        if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-        return comptime blk: {
-            const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-            break :blk root_dir ++ suffix;
-        };
-    }
-};
 
 comptime {
     const min_zig = std.SemanticVersion.parse("0.11.0-dev.4410+76f7b40e1") catch unreachable;
