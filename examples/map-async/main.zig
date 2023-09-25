fd6694c9d9c35ac63bc3b49f895dbaf9a4fa7c3c
Patrick Doane <pdoane2@gmail.com>
2023-09-25 10:33:03 -0700

    examples: map-async fixes

--- examples/map-async/main.zig
+++ examples/map-async/main.zig
@@ -18,12 +18,14 @@ pub fn init(app: *App) !void {
         .size = buffer_size * @sizeOf(f32),
         .mapped_at_creation = .false,
     });
+    defer output.release();
 
     const staging = core.device.createBuffer(&.{
         .usage = .{ .map_read = true, .copy_dst = true },
         .size = buffer_size * @sizeOf(f32),
         .mapped_at_creation = .false,
     });
+    defer staging.release();
 
     const compute_module = core.device.createShaderModuleWGSL("main.wgsl", @embedFile("main.wgsl"));
 
@@ -31,13 +33,18 @@ pub fn init(app: *App) !void {
         .module = compute_module,
         .entry_point = "main",
     } });
+    defer compute_pipeline.release();
+
+    const layout = compute_pipeline.getBindGroupLayout(0);
+    defer layout.release();
 
     const compute_bind_group = core.device.createBindGroup(&gpu.BindGroup.Descriptor.init(.{
-        .layout = compute_pipeline.getBindGroupLayout(0),
+        .layout = layout,
         .entries = &.{
-            gpu.BindGroup.Entry.buffer(0, output, 0, buffer_size),
+            gpu.BindGroup.Entry.buffer(0, output, 0, buffer_size * @sizeOf(f32)),
         },
     }));
+    defer compute_bind_group.release();
 
     compute_module.release();
 
@@ -48,8 +55,9 @@ pub fn init(app: *App) !void {
     compute_pass.setBindGroup(0, compute_bind_group, &.{});
     compute_pass.dispatchWorkgroups(try std.math.divCeil(u32, buffer_size, workgroup_size), 1, 1);
     compute_pass.end();
+    compute_pass.release();
 
-    encoder.copyBufferToBuffer(output, 0, staging, 0, buffer_size);
+    encoder.copyBufferToBuffer(output, 0, staging, 0, buffer_size * @sizeOf(f32));
 
     var command = encoder.finish(null);
     encoder.release();
@@ -63,8 +71,9 @@ pub fn init(app: *App) !void {
 
     var queue = core.queue;
     queue.submit(&[_]*gpu.CommandBuffer{command});
+    command.release();
 
-    staging.mapAsync(.{ .read = true }, 0, buffer_size, &response, callback);
+    staging.mapAsync(.{ .read = true }, 0, buffer_size * @sizeOf(f32), &response, callback);
     while (true) {
         if (response == gpu.Buffer.MapAsyncStatus.success) {
             break;
@@ -73,7 +82,7 @@ pub fn init(app: *App) !void {
         }
     }
 
-    const staging_mapped = staging.getConstMappedRange(f32, 0, buffer_size / @sizeOf(f32));
+    const staging_mapped = staging.getConstMappedRange(f32, 0, buffer_size);
     for (staging_mapped.?) |v| {
         std.debug.print("{d} ", .{v});
     }
